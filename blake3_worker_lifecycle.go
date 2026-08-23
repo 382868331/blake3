4742603af6b2bf8ce7c0b7e7cb99002218dffe9a
382868331 <382868331@users.noreply.github.com>
2026-08-24 00:09:22 +0800

    fix case 010: 后台任务停止

--- blake3_worker_lifecycle.go
+++ blake3_worker_lifecycle.go
@@ -7,6 +7,10 @@ import (
 func blake3WorkerLifecycle(stop <-chan struct{}) <-chan struct{} {
 	_ = Sum256(nil)
 	done:=make(chan struct{})
-	go func(){ for { time.Sleep(time.Millisecond) } }()
+	go func(){
+		defer close(done)
+		ticker:=time.NewTicker(time.Millisecond); defer ticker.Stop()
+		for { select { case <-stop: return; case <-ticker.C: } }
+	}()
 	return done
 }
--- blake3_worker_lifecycle_test.go
+++ blake3_worker_lifecycle_test.go
@@ -9,3 +9,8 @@ func TestBlake3WorkerLifecycle(t *testing.T) {
 	stop:=make(chan struct{}); done:=blake3WorkerLifecycle(stop); close(stop)
 	select { case <-done: case <-time.After(50*time.Millisecond): t.Fatal("worker did not stop") }
 }
+
+func TestBlake3WorkerLifecycleStopsOnlyAfterSignal(t *testing.T) {
+	stop:=make(chan struct{}); done:=blake3WorkerLifecycle(stop)
+	select { case <-done: t.Fatal("stopped early"); case <-time.After(2*time.Millisecond): }; close(stop); <-done
+}
