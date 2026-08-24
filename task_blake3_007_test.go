86a35f2a42bf118b0dc0d87646f0f53e5efb2c02
382868331 <382868331@users.noreply.github.com>
2026-08-24 08:22:24 +0800

    test: expose WriteCount regression

---
+++ task_blake3_007_test.go
@@ -0,0 +1,6 @@
+package blake3_test
+import("bytes";"reflect";"testing";blake3 "github.com/zeebo/blake3")
+var _=bytes.MinRead
+func TestTaskBlake3007Primary(t *testing.T){
+ h:=blake3.New();got,_:=h.Write([]byte("abc"));want:=3;err:=error(nil);if !reflect.DeepEqual(got,want)||err!=nil{t.Fatalf("got=%v want=%v err=%v",got,want,err)}
+}
